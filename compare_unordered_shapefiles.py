68c1c95a5a4c5ea46109d52377399794e1abb058
Kayla Hardie <kaylahardie7@gmail.com>
2024-04-12 21:45:54 +0000

    Add documentation for compare_unordered_shapefiles

--- compare_unordered_shapefiles.py
+++ compare_unordered_shapefiles.py
@@ -1,24 +1,32 @@
-import fiona
+"""
+Compares the features in two shapefiles. The geometry and properties for each feature are compared. 
+The features don't have to be in the same order in the two files for them to be equal. This approach
+assumes that each feature in the file is unique (ie. it doesn't guarantee equality if there are 
+duplicate features in one of the files.)
+"""
 import argparse
 import sys
+
+import fiona
 from shapely.geometry import shape
+
 
 def feature_to_hashable(feature):
     """Converts a feature into a hashable representation."""
-    geometry_repr = shape(feature['geometry']).wkt  # Represent geometry as WKT
-    attributes = tuple(sorted(feature['properties'].items()))  # Sort attributes
+    geometry_repr = shape(feature['geometry']).wkt
+    attributes = tuple(sorted(feature['properties'].items()))
     return (geometry_repr, attributes)
 
+
 def compare_unordered_shapefiles(file1, file2):
-    """Compares shapefiles using the set approach."""
+    """Compares shapefiles using sets. This approach doesn't work if there are duplicate features in one of the files."""
     with fiona.open(file1) as src1, fiona.open(file2) as src2:
         set1 = {feature_to_hashable(feature) for feature in src1}
         set2 = {feature_to_hashable(feature) for feature in src2}
 
         if len(set1) != len(set2):
-            print("{} doesn't have the same number of elements: {}, as {}: {}".format(
-                file1, len(set1), file2, len(set2)
-            ))
+            print(
+                f"{file1} doesn't have the same number of elements: {len(set1)}, as {file2}: {len(set2)}")
             return
 
         if set1 == set2:
@@ -32,8 +40,10 @@ def compare_unordered_shapefiles(file1, file2):
         print(f"{len(diff1)} features unique to {file1}")
         print(f"{len(diff2)} features unique to {file2}")
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Compare the features in two shapefiles.")
+    parser = argparse.ArgumentParser(
+        description="Compare the features in two shapefiles.")
     parser.add_argument("file1", help="Path to the first shapefile")
     parser.add_argument("file2", help="Path to the second shapefile")
 
@@ -47,5 +57,4 @@ if __name__ == "__main__":
     compare_unordered_shapefiles(args.file1, args.file2)
 
 # Usage:
-#compare_unordered_shapefiles("/usr/local/google/home/kaylahardie/MAPLE_v3/data/ray_shapefiles/test_image_01.shp", "/usr/local/google/home/kaylahardie/MAPLE_v3/data/projected_shp/test_image_01/test_image_01.shp")
-#compare_unordered_shapefiles("/usr/local/google/home/kaylahardie/MAPLE_v3/data/projected_shp copy/test_image_01/test_image_01.shp", "/usr/local/google/home/kaylahardie/MAPLE_v3/data/projected_shp/test_image_01/test_image_01.shp")  
+# python compare_unordered_shapefiles.py <path to file1> <path to file2>
